80630fb3b38832a1ab4b66e177c8c1cd0fb65fe6
ChongLiQuan <p19011389@student.newinti.edu.my>
2022-05-22 11:29:22 +0800

    Added a temporary add button
    Added button without backend connection yet

--- 302CEM_BS/resources/views/welcome.blade.php
+++ 302CEM_BS/resources/views/welcome.blade.php
@@ -17,9 +17,9 @@
                     <a href="book_cover"><img src=images/{{$book->book_cover_img}} height='250' width='150'></a>
                     <h4> {{ $book->book_title }} </h4>              
                     <h4>Price: {{ $book->retail_price }} </h4>
-                    
-                    <a href="add_to_cart">Add To Cart</a>
- 
+
+                    <button> Add To Cart </button>
+                     
                     </center>          
                 </div>
 
