8bec06eff3519c28fa6f3e5dc4320ddb3e77409f
ZacOT <62330525+ZacOT@users.noreply.github.com>
2022-06-10 12:10:13 +0800

    Bug Fixing
    fixing merge bug

--- 302CEM_BS/resources/views/welcome.blade.php
+++ 302CEM_BS/resources/views/welcome.blade.php
@@ -25,45 +25,43 @@
                     
                       if($role == 1){
                         echo" <h4>Retail Price: $book->retail_price </h4> ";
-                        echo" <button> Add To Cart </button> ";
-                        }
-
-                    </center>          
-
-                @if(Auth::user())
-
-                    <form action = {{route("insertCart")}} method ='post' class='form-group' enctype='multipart/form-data' align='center'>
-                    <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
-
-                    <input type="hidden" class="form-control" name="username" value="{{Auth::user()->username}}">
-                    <input type="hidden" class="form-control" name="ISBN_13" value="{{$book->ISBN_13}}">
-                    <input type="hidden" class="form-control" name="book_quantity" value=1>
-
-                    <button type="submit">Add To Cart</button>
-                    </form>
-
+                        echo" <button> Add To Cart </button> ";?>
+                        
+                        <form action = {{route("insertCart")}} method ='post' class='form-group' enctype='multipart/form-data' align='center'>
+                        <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
+    
+                        <input type="hidden" class="form-control" name="username" value="{{Auth::user()->username}}">
+                        <input type="hidden" class="form-control" name="ISBN_13" value="{{$book->ISBN_13}}">
+                        <input type="hidden" class="form-control" name="book_quantity" value=1>
+    
+                        <?php echo"
+                        <button type='submit'>Add To Cart</button>
+                        </form>";
+                        
+                      }
+                    }
+                ?>
                     </center>
                 </div>
+              
+                
+
+                <?php
                       if($role == 0){
                         echo" <h4>ISBN_13: $book->ISBN_13 </h4> ";
                         echo" <h4>Stock Quantity: $book->book_stock </h4> ";
-                        ?> 
+                      }
+                ?> 
                         <form action = "{{route('deleteBook')}}" method='GET' class='form-group' action='/' enctype='multipart/form-data'>
                         <input type = 'hidden' name = '_token' value = '<?php echo csrf_token(); ?>'>
                         <input type = 'hidden' name = 'delete_isbn13' value="{{ $book->ISBN_13 }}">
                         
-                        <?php echo"
+                <?php echo"
                         <button type='submit'>Delete Book</button>
-                        </form>";
-                        
-
-                        }     
-                    }
+                        </form>";   
                 ?> 
                     </center>          
                     </div>
-
-                @endif
 
                 @endforeach
                 
